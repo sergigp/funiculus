3f5c18f277607b506c2421cb757183cfc5133d53
sergigp <sergi.gonzalez@akamon.com>
2014-08-19 09:02:44 +0200

    dummy test

--- tests/DummyTest.php
+++ tests/DummyTest.php
@@ -1,10 +1,10 @@
 <?php
 
-class DummyTest 
+class DummyTest extends PHPUnit_Framework_TestCase
 {
     /** @test **/
     public function it_should_return_true()
     {
-        
+        $this->assertTrue(true);
     }
 }
